5f9e0c23e39d010d6593fe5c597eab3ce132264a
Elroi <itsxelliex@gmail.com>
2025-11-28 12:21:07 -0500

    hi

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 864
+screen_height = 936
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Angel')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    
+    
+pygame.quit()
