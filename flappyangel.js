433d29bfb230f9e9ed59deac1131448874425821
Elroi <itsxelliex@gmail.com>
2025-11-28 15:57:14 -0500

    jj

---
+++ flappyangel.js
@@ -0,0 +1,15 @@
+
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); // used for drawing on the board
+    context.fillStyle = "skyblue";
+    context.fillRect(0, 0, boardWidth, boardHeight); // fill the board with skyblue color
+}
